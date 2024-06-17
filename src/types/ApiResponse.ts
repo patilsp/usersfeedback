ba7a8ae96fe008d0cb3b5fb0eb0ec3f5f2dacfe8
unknown <Dell@RN-BLR-LT-18.Fugo001.net>
2024-06-17 11:05:41 +0530

    resend aip changes

---
+++ src/types/ApiResponse.ts
@@ -0,0 +1,8 @@
+import { Messages } from "@/model/user";
+
+export interface ApiResponse{
+    success: boolean;
+    message: string;
+    isAcceptingMessages?: boolean;
+    messages?: Array<Message>;
+}
